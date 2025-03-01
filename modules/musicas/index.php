15494f132bc9f14ae1bbcceb5308c040ef352d34
Daniel Segatto <dnsegatto1@gmail.com>
2025-03-01 15:59:03 -0300

    Atualizado

--- modules/musicas/index.php
+++ modules/musicas/index.php
@@ -54,6 +54,7 @@ if(isset($tracks['items'][0]['track']['id'])){
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <!-- Font Awesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+    
     <style>
         body {
             background-color: #02416d;
@@ -551,7 +552,6 @@ if(isset($tracks['items'][0]['track']['id'])){
     </script>
 </head>
 <body>
-    <?php include '../../includes/header.php'; ?>
     <!-- Cabeçalho da Playlist -->
     <div class="playlist-header">
         <div class="container">
